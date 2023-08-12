8682736f3ef219173d1e0171971fe9bf76e404c7
LostDeception <tmickelson93@gmail.com>
2023-08-11 20:13:37 -0500

    remove clientside prediction (temp)

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -180,25 +180,25 @@ setInterval(() => {
   if(keys.w.pressed) {
     sequenceNumber++;
     playerInputs.push({sequenceNumber, dx: 0, dy: -SPEED});
-    frontEndPlayers[socket.id].y -= SPEED;
+    //frontEndPlayers[socket.id].y -= SPEED;
     socket.emit('keydown', { keycode: 'KeyW', sequenceNumber });
   }
   if(keys.a.pressed) {
     sequenceNumber++;
     playerInputs.push({sequenceNumber, dx: -SPEED, dy: 0});
-    frontEndPlayers[socket.id].x -= SPEED;
+    //frontEndPlayers[socket.id].x -= SPEED;
     socket.emit('keydown', { keycode: 'KeyA', sequenceNumber });
   }
   if(keys.s.pressed) { 
     sequenceNumber++;
     playerInputs.push({sequenceNumber, dx: 0, dy: SPEED});
-    frontEndPlayers[socket.id].y += SPEED;
+    //frontEndPlayers[socket.id].y += SPEED;
     socket.emit('keydown', { keycode: 'KeyS', sequenceNumber });
   }
   if(keys.d.pressed) {
     sequenceNumber++;
     playerInputs.push({sequenceNumber, dx: SPEED, dy: 0});
-    frontEndPlayers[socket.id].x += SPEED;
+    //frontEndPlayers[socket.id].x += SPEED;
     socket.emit('keydown', { keycode: 'KeyD', sequenceNumber });
   }
 
